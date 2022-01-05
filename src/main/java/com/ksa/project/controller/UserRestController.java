8d84621072869183bd376dadc72e03aff93ab7a7
commGom <hyunwoo921129@gmail.com>
2022-01-05 19:18:52 +0900

    로그인기능 UserRestController에 우선적으로 구현

---
+++ src/main/java/com/ksa/project/controller/UserRestController.java
@@ -0,0 +1,132 @@
+package com.ksa.project.controller;
+
+import com.ksa.project.model.User;
+import com.ksa.project.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/user/api")
+public class UserRestController {
+	@Autowired
+	UserRepository userRepository;
+
+//	// 이메일 찾기
+//	@GetMapping("/findEmail")
+//	public String findEmail() {
+//		return "/user/findEmail";
+//	}
+//
+//	@PostMapping("/findEmail")
+//	@ResponseBody
+//	public Map<String, Object> findEmailPost(@ModelAttribute User user) {
+//		Map<String, Object>  result = new HashMap<>();
+//		System.out.println(user);
+//
+//		User dbUser = userRepository.findByNameAndPhone(user.getName(), user.getPhone());
+//
+//			if (dbUser == null ) {
+//				result.put("msg", "가입된 정보가 없습니다.");
+//				result.put("code", 201);
+//			} else {
+//				result.put("msg", dbUser.getEmail());
+//				result.put("code", 200);
+//			}
+//			return result;
+//	}
+//	// 비밀번호 찾기
+//	@GetMapping("/findPassword")
+//	public String findPassword() {
+//		return "/user/findPassword";
+//	}
+//
+//	@PostMapping("/findPassword")
+//	@ResponseBody
+//	public Map<String, Object> findPasswordPost(@ModelAttribute User user) {
+//		Map<String, Object>  result = new HashMap<>();
+//		System.out.println(user);
+//
+//		User dbUser = userRepository.findByEmail(user.getEmail());
+//
+//			if (dbUser == null ) {
+//				result.put("msg", "가입된 정보가 없습니다.");
+//				result.put("code", 201);
+//			} else {
+//				result.put("msg", dbUser.getEmail());
+//				result.put("code", 200);
+//			}
+//			return result;
+//	}
+//
+//	// 아이디 체크
+//	@PostMapping("/emailCheck")
+//	@ResponseBody
+//	public User emailCheck(@RequestParam("email") String email, User user) {
+//		log.info("userEmailCheck 진입");
+//		log.info("전달받은 email:" + email);
+//		User dbUser = userRepository.findByEmail(user.getEmail());
+//		log.info("확인 결과:" + dbUser);
+//		return dbUser;
+//	}
+//
+//	@GetMapping("/signin")
+//	public String signin() {
+//		return "user/signin";
+//	}
+
+	//로그인 기능 구현
+	@PostMapping("/signin")
+	public Map<String, Object> signinPost(String email, String password) {
+		Map<String, Object> map = new HashMap<>();
+		User dbUser = userRepository.findByEmailAndPassword(
+				email, password);
+
+		if (dbUser != null) {
+			System.out.println("로그인 성공");
+			map.put("user",dbUser);
+			map.put("code",200);
+			map.put("msg", dbUser.getName()+"님❤\n 안녕하세요!️");
+		} else {
+			map.put("code",400);
+			map.put("msg", "잘못 된 정보 입력, 로그인 실패");
+		}
+		return map;
+	}
+
+	@GetMapping("/signout")
+	public String signout() {
+		return "redirect:/";
+	}
+
+	@GetMapping("/signup")
+	public String signup() {
+		return "user/signup";
+	}
+
+	@PostMapping("/signup")
+	@ResponseBody
+	public Map<String, Object> signupPost(@ModelAttribute User user) {
+		System.out.println(user);
+		Map<String, Object> result = new HashMap<>();
+		User dbUser = userRepository.findByEmail(user.getEmail());
+		if (dbUser != null) {
+			System.out.println("회원가입 실패");
+			result.put("msg", "회원가입 실패");
+			result.put("code", 201);
+		} else {
+			userRepository.save(user);
+			System.out.println("회원가입 성공");
+			result.put("msg", "회원가입 성공");
+			result.put("code", 200);
+		}
+		return result;
+
+	}
+};
