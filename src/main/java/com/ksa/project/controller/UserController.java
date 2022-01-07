2d077a43a6e18feb5289382561a6207bbf59027a
YangDamin <cxz26@naver.com>
2022-01-07 11:55:04 +0900

    회원가입 수정

--- src/main/java/com/ksa/project/controller/UserController.java
+++ src/main/java/com/ksa/project/controller/UserController.java
@@ -10,6 +10,7 @@ import com.ksa.project.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @Slf4j
 @RequestMapping("/user")
+@CrossOrigin
 public class UserController {
 	@Autowired
 	UserRepository userRepository;
